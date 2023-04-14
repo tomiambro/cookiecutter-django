01c2602a56ddf795edcbaccf30b72457af9ff136
Tomas Ambrogi <tomi.ambro94@gmail.com>
2023-04-14 16:51:53 -0300

    adding example base task class

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/example_tasks.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/example_tasks.py
@@ -1,7 +1,23 @@
 # import logging
 
 # from celery import current_app as app
+# from celery import Task
 # from django.db import IntegrityError
 
+# class BaseTask(Task):
+#     soft_time_limit = 60 * 15  # in seconds, 15 minutes
+#     time_limit = soft_time_limit + 30  # in seconds, 15.5 minutes
+#     ignore_result = False
 
-# @app.task()
+#     def before_start(self, task_id, args, kwargs):
+#         pass
+
+#     def after_return(self, status, retval, task_id, args, kwargs, einfo):
+#         # Celery uses confusing function signatures...
+#         # If the task succeeds the arguments are as expected.
+#         # If the task fails the error is in the retval and the einfo parameter is empty.
+#         if not isinstance(retval, Exception):
+#             pass
+
+
+# @app.task(base=BaseTask)
